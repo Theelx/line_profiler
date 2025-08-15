1478966e08cd59c96719bfd257f7fd45d8d7f4e4
Terence S.-C. Tsang <t.tsang.sci@gmail.com>
2025-08-15 04:40:21 +0200

    Stub

--- line_profiler/ipython_extension.pyi
+++ line_profiler/ipython_extension.pyi
@@ -1,7 +1,15 @@
 from IPython.core.magic import Magics
+from . import LineProfiler
 
 
 class LineProfilerMagics(Magics):
+    def parse_parameters(self, parameter_s: str) -> str:
+        ...
 
-    def lprun(self, parameter_s: str = ...):
+    def lprun(self, parameter_s: str = ...) -> LineProfiler | None:
+        ...
+
+    def lprun_all(self,
+                  parameter_s: str = "",
+                  cell: str | None = None) -> LineProfiler | None:
         ...
